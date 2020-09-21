b20378ae80830b76b23a9df0a4b7437364e67276
TL-20191123VLYG\TongLe <1919777283@qq.com>
2020-09-21 14:23:39 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+// import Login from '../views/user/login/index.vue'
+// import Admin from '../views/user/admin/index.vue'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect:'/login'
+  },
+  {
+    path: '/login',
+    name:'Login',
+  	component:Login,
+  },
+  {
+    path: '/admin',
+    name:'Admin',
+	component:Admin,
   },
   {
     path: '/about',
